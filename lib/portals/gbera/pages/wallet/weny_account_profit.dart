b7c26d299b4991ce7727996656160da3b4608488
cj <cj@ss.com>
2020-09-15 17:11:36 +0800

    2.0.0

--- lib/portals/gbera/pages/wallet/weny_account_profit.dart
+++ lib/portals/gbera/pages/wallet/weny_account_profit.dart
@@ -1,10 +1,6 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:framework/core_lib/_page_context.dart';
 import 'package:netos_app/portals/gbera/store/remotes/wallet_accounts.dart';
-import 'package:netos_app/portals/gbera/store/remotes/wallet_records.dart';
-import 'package:netos_app/portals/gbera/store/remotes/wallet_trades.dart';
 
 class ProfitWenyAccount extends StatefulWidget {
   PageContext context;
@@ -17,58 +13,12 @@ class ProfitWenyAccount extends StatefulWidget {
 
 class _ProfitWenyAccountState extends State<ProfitWenyAccount> {
   WenyBank _bank;
-  bool _enableButton = false;
-  String _buttonText = '提取到零钱';
   GlobalKey<ScaffoldState> _key = GlobalKey();
 
   @override
   void initState() {
     _bank = widget.context.parameters['bank'];
-    _enableButton = _bank.profit > 0;
     super.initState();
-  }
-
-  Future<void> _transProfit() async {
-    _enableButton = false;
-    _buttonText = '提取中...';
-    if (mounted) {
-      setState(() {});
-    }
-    IWalletTradeRemote tradeRemote =
-        widget.context.site.getService('/wallet/trades');
-    IWalletRecordRemote recordRemote =
-        widget.context.site.getService('/wallet/records');
-    TransProfitResult result =
-        await tradeRemote.transProfit(_bank.bank, _bank.profit, '');
-    Timer.periodic(
-        Duration(
-          seconds: 1,
-        ), (timer) async {
-      TransProfitOR record;
-      try {
-        record = await recordRemote.getTransProfit(result.sn);
-      } catch (ex) {
-        timer.cancel();
-        throw FlutterError(ex);
-      }
-      if (record.state == 1) {
-        timer.cancel();
-      }
-      if (result.status < 300) {
-        _bank.profit = 0;
-        _buttonText = '成功';
-      } else {
-        _buttonText = '失败';
-      }
-      if (mounted) {
-        setState(() {});
-      }
-      String _message = '${result.status} ${result.message}';
-
-      _key.currentState.showSnackBar(SnackBar(
-        content: Text('$_message'),
-      ));
-    });
   }
 
   @override
@@ -133,17 +83,18 @@ class _ProfitWenyAccountState extends State<ProfitWenyAccount> {
             width: 160,
             height: 36,
             child: RaisedButton(
-              onPressed: !_enableButton
-                  ? null
-                  : () {
-                      _transProfit();
+              onPressed:() {
+                widget.context.forward(
+                  '/wybank/bill/profit',
+                  arguments: {'bank': _bank},
+                );
                     },
               textColor: Colors.white,
               color: Colors.green,
               disabledColor: Colors.grey[300],
               disabledTextColor: Colors.grey[400],
               highlightColor: Colors.green[600],
-              child: Text(_buttonText),
+              child: Text('查看明细'),
             ),
           ),
         ),
@@ -160,15 +111,6 @@ class _ProfitWenyAccountState extends State<ProfitWenyAccount> {
         elevation: 0,
         automaticallyImplyLeading: true,
         actions: <Widget>[
-          FlatButton(
-            onPressed: () {
-              widget.context.forward(
-                '/wybank/bill/profit',
-                arguments: {'bank': _bank},
-              );
-            },
-            child: Text('明细'),
-          ),
         ],
       ),
       body: Container(
