39d0ba890bd8b749a78e30a914751bc8bc78bd01
cj <cj@ss.com>
2020-12-14 01:14:59 +0800

    0.1.7

--- lib/portals/gbera/pages/desktop.dart
+++ lib/portals/gbera/pages/desktop.dart
@@ -1,11 +1,15 @@
 import 'dart:io';
 
+import 'package:badges/badges.dart';
+import 'package:custom_pop_up_menu/custom_pop_up_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_easyrefresh/easy_refresh.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:framework/framework.dart';
 import 'package:netos_app/common/portlet_market.dart';
 import 'package:netos_app/common/qrcode_scanner.dart';
+import 'package:netos_app/common/util.dart';
 import 'package:netos_app/portals/gbera/pages/profile/qrcode.dart' as person;
 import 'package:netos_app/portals/gbera/pages/wallet/receivables.dart'
     as receivables;
@@ -27,6 +31,9 @@ class _DesktopState extends State<Desktop> with AutomaticKeepAliveClientMixin {
   List<Widget> _desklets = [];
   bool _isloaded = false;
   EasyRefreshController _controller;
+  CustomPopupMenuController _customPopupMenuController =
+      CustomPopupMenuController();
+  bool _isChecked = false;
 
   @override
   bool get wantKeepAlive {
@@ -36,7 +43,7 @@ class _DesktopState extends State<Desktop> with AutomaticKeepAliveClientMixin {
   @override
   void initState() {
     _controller = EasyRefreshController();
-    _load().then((v) async{
+    _load().then((v) async {
       _isloaded = true;
       if (mounted) {
         setState(() {});
@@ -49,6 +56,7 @@ class _DesktopState extends State<Desktop> with AutomaticKeepAliveClientMixin {
   @override
   void dispose() {
     _controller.dispose();
+    _customPopupMenuController?.dispose();
     _desklets.clear();
     _isloaded = false;
     super.dispose();
@@ -248,6 +256,66 @@ class _DesktopState extends State<Desktop> with AutomaticKeepAliveClientMixin {
             backgroundColor: Colors.transparent,
             toolbarOpacity: 1,
             actions: <Widget>[
+              Badge(
+                position: BadgePosition.bottomStart(
+                  bottom: 0,
+                  start: 0,
+                ),
+                elevation: 0,
+                showBadge: true,
+                badgeContent: Text(
+                  '',
+                ),
+                child: CustomPopupMenu(
+                  controller: _customPopupMenuController,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                    ),
+                    child: Icon(
+                      Icons.lightbulb_outline_sharp,
+                      size: 25,
+                    ),
+                  ),
+                  menuBuilder: () {
+                    return Container(
+                      padding: EdgeInsets.only(
+                        left: 10,
+                        right: 10,
+                        bottom: 10,
+                        top: 15,
+                      ),
+                      margin: EdgeInsets.only(
+                        left: 10,
+                        right: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Color(0xefFFFFFF),
+                        borderRadius: BorderRadius.circular(4),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(
+                              2,
+                              2,
+                            ),
+                            blurRadius: 4,
+                          ),
+                        ],
+                      ),
+                      constraints: BoxConstraints(
+                        minHeight: 100,
+                        minWidth: 200,
+                      ),
+                      child: _renderTiptool(),
+                    );
+                  },
+                  arrowColor: Colors.black38,
+                  barrierColor: Colors.transparent,
+                  pressType: PressType.singleClick,
+                ),
+              ),
               IconButton(
                 // Use the FontAwesomeIcons class for the IconData
                 icon: new Icon(Icons.crop_free),
@@ -283,6 +351,157 @@ class _DesktopState extends State<Desktop> with AutomaticKeepAliveClientMixin {
         ),
         Expanded(
           child: myarea,
+        ),
+      ],
+    );
+  }
+
+  Widget _renderTiptool() {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.only(
+            left: 10,
+            right: 10,
+            top: 5,
+            bottom: 5,
+          ),
+          decoration: BoxDecoration(
+            color: Color(0xeef5f5f5),
+            // color: Colors.yellow,
+          ),
+          child: Row(
+            children: [
+              SizedBox(
+                width: 55,
+                height: 55,
+                child: getAvatarWidget(
+                  widget.context.principal.avatarOnRemote,
+                  widget.context,
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(
+                      '新冠疫苗，囚犯和胖子优先！澳大利亚网友炸锅了',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      '据环球时报援引澳大利亚媒体9NEWS新闻网12月10日报道，与老年人、孕妇、医护人员和慢性肥胖症患者一道，监狱中的囚犯和教养人员，将成为澳大利亚首批接种新冠疫苗的人群。因为该国卫生部门认为，囚犯的免疫力比大多数的普通人都要弱。',
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        _CheckBoxWidget(),
+      ],
+    );
+  }
+}
+
+class _CheckBoxWidget extends StatefulWidget {
+  @override
+  __CheckBoxWidgetState createState() => __CheckBoxWidgetState();
+}
+
+class __CheckBoxWidgetState extends State<_CheckBoxWidget> {
+  bool _isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        InkWell(
+          onTap: () {
+            setState(() {
+              _isChecked = !_isChecked;
+            });
+          },
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    // shape: BoxShape.circle,
+                    // color: Colors.blue,
+                    ),
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    left: 10,
+                    right: 10,
+                  ),
+                  child: _isChecked
+                      ? Icon(
+                          Icons.check,
+                          size: 14.0,
+                          color: Colors.green,
+                        )
+                      : Icon(
+                          Icons.check_box_outline_blank,
+                          size: 14.0,
+                          color: Colors.grey,
+                        ),
+                ),
+              ),
+              Text(
+                '不再自动弹出',
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        InkWell(
+          onTap: () {
+            setState(() {
+            });
+          },
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                '下一提示',
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Icon(
+                Icons.skip_next,
+                size: 14,
+              ),
+            ],
+          ),
         ),
       ],
     );
