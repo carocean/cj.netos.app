24f399fe7a39c21b9665076b5a48f4081114eac0
cj <cj@ss.com>
2020-12-15 01:57:40 +0800

    0.1.9

--- lib/portals/gbera/pages/system/tiptool_view.dart
+++ lib/portals/gbera/pages/system/tiptool_view.dart
@@ -20,6 +20,7 @@ class _TipsDocPreviewAndCreateState extends State<TipsDocPreviewAndCreate> {
   String _title;
   String _summary;
   String _href;
+  double _progress = 0.0;
 
   @override
   void initState() {
@@ -27,6 +28,7 @@ class _TipsDocPreviewAndCreateState extends State<TipsDocPreviewAndCreate> {
     _leading = widget.context.partArgs['leading'];
     _href = widget.context.partArgs['href'];
     _summary = widget.context.partArgs['summary'];
+    _checkAndUploadLeading();
     super.initState();
   }
 
@@ -34,6 +36,27 @@ class _TipsDocPreviewAndCreateState extends State<TipsDocPreviewAndCreate> {
   void dispose() {
     // TODO: implement dispose
     super.dispose();
+  }
+
+  Future<void> _checkAndUploadLeading() async {
+    if (StringUtil.isEmpty(_leading)) {
+      return;
+    }
+    if (!_leading.startsWith('/')) {
+      return;
+    }
+    var map = await widget.context.ports.upload('/app/feedback/', [_leading],
+        onSendProgress: (i, j) {
+      _progress = ((i * 1.0) / j) * 100.00;
+      if (mounted) {
+        setState(() {});
+      }
+    });
+    _leading = map[_leading];
+    _progress=0.0;
+    if (mounted) {
+      setState(() {});
+    }
   }
 
   Future<void> _create() async {
@@ -66,6 +89,7 @@ class _TipsDocPreviewAndCreateState extends State<TipsDocPreviewAndCreate> {
 
   bool _checkButton() {
     return !StringUtil.isEmpty(_leading) &&
+        !_leading.startsWith('/') &&
         !StringUtil.isEmpty(_title) &&
         !StringUtil.isEmpty(_summary) &&
         !StringUtil.isEmpty(_href);
@@ -104,13 +128,30 @@ class _TipsDocPreviewAndCreateState extends State<TipsDocPreviewAndCreate> {
               ),
               child: Row(
                 children: [
-                  SizedBox(
-                    width: 55,
-                    height: 55,
-                    child: getAvatarWidget(
-                      _leading,
-                      widget.context,
-                    ),
+                  Column(
+                    children: [
+                      SizedBox(
+                        width: 55,
+                        height: 55,
+                        child: getAvatarWidget(
+                          _leading,
+                          widget.context,
+                        ),
+                      ),
+                      _progress > 0
+                          ? Center(
+                              child: Text(
+                                '${_progress.toStringAsFixed(2)}%',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                ),
+                              ),
+                            )
+                          : SizedBox(
+                              width: 0,
+                              height: 0,
+                            ),
+                    ],
                   ),
                   SizedBox(
                     width: 10,
