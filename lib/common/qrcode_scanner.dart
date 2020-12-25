eaa7a0ec16239c9061b984475d266d04b5e185fd
cj <cj@ss.com>
2020-12-25 16:13:03 +0800

    0.2.0

--- lib/common/qrcode_scanner.dart
+++ lib/common/qrcode_scanner.dart
@@ -1,33 +1,33 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:framework/framework.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:r_scan/r_scan.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 QrcodeScanner qrcodeScanner = QrcodeScanner(
   actions: {},
 );
-final scanner=_DefaultScanner();
+final scanner = _DefaultScanner();
 
-mixin IScanner{
+mixin IScanner {
   Future<String> scan(PageContext context);
 }
-class _DefaultScanner implements IScanner{
+
+class _DefaultScanner implements IScanner {
   @override
-  Future<String> scan(PageContext context)async{
-    var result= await context.forward('/qrcode/scanner');
-    if(result==null) {
+  Future<String> scan(PageContext context) async {
+    var result = await context.forward('/qrcode/scanner');
+    if (result == null) {
       return null;
     }
-    var s=result as RScanResult;
-    return s.message;
+    var s = result as Barcode;
+    return s.code;
   }
 }
+
 class QrcodeScanner {
   final Map<String, QrcodeAction> actions;
 
@@ -160,53 +160,26 @@ class QrScannerDialog extends StatefulWidget {
 }
 
 class _QrScannerDialogState extends State<QrScannerDialog> {
-  RScanCameraController _controller;
+  final GlobalKey _qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController _controller;
   bool isFirst = true;
+  var _flashState = false;
 
-  List<RScanCameraDescription> rScanCameras;
-
-  void initCamera() async {
-    if (rScanCameras == null || rScanCameras.length == 0) {
-      final result = await PermissionHandler()
-          .checkPermissionStatus(PermissionGroup.camera);
-      if (result == PermissionStatus.granted) {
-        rScanCameras = await availableRScanCameras();
-        print('返回可用的相机：${rScanCameras.join('\n')}');
-      } else {
-        final resultMap = await PermissionHandler()
-            .requestPermissions([PermissionGroup.camera]);
-        if (resultMap[PermissionGroup.camera] == PermissionStatus.granted) {
-          rScanCameras = await availableRScanCameras();
-        } else {
-          print('相机权限被拒绝，无法使用');
-        }
-      }
-    }
-    if (rScanCameras != null && rScanCameras.length > 0) {
-      _controller = RScanCameraController(
-          rScanCameras[0], RScanCameraResolutionPreset.high)
-        ..addListener(() {
-          final result = _controller.result;
-          if (result != null) {
-            if (isFirst) {
-              Navigator.of(context).pop(result);
-              isFirst = false;
-            }
-          }
-        })
-        ..initialize().then((_) {
-          if (!mounted) {
-            return;
-          }
-          setState(() {});
-        });
+// In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      _controller.pauseCamera();
+    } else if (Platform.isIOS) {
+      _controller.resumeCamera();
     }
   }
 
   @override
   void initState() {
     super.initState();
-    initCamera();
   }
 
   @override
@@ -214,213 +187,104 @@ class _QrScannerDialogState extends State<QrScannerDialog> {
     _controller?.dispose();
     super.dispose();
   }
-  Future<void> _scanImage()async{
-    var picker=ImagePicker();
-   var image=await picker.getImage(source: ImageSource.gallery,imageQuality: 100,);
-    if(image==null) {
-      return;
-    }
-    final result = await RScan.scanImagePath(image.path);
-    Navigator.of(context).pop(result);
-  }
+
   @override
   Widget build(BuildContext context) {
-    if (rScanCameras == null || rScanCameras.length == 0) {
-      return Scaffold(
-        body: Container(
-          alignment: Alignment.center,
-          child: Text('not have available camera'),
-        ),
-      );
-    }
-    if (!_controller.value.isInitialized) {
-      return Container();
-    }
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 220.0
+        : 300.0;
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.black,
-        iconTheme: IconThemeData(color: Colors.white,),
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.image,
-            ),
-            onPressed: () {
-              _scanImage();
-            },
-          ),
-        ],
-      ),
       backgroundColor: Colors.black,
       body: Stack(
-        children: <Widget>[
-          ScanImageView(
-            child: AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              child: RScanCamera(_controller),
+        children: [
+          NotificationListener<SizeChangedLayoutNotification>(
+            onNotification: (notification) {
+              Future.microtask(() =>
+                  _controller?.updateDimensions(_qrKey, scanArea: scanArea));
+              return false;
+            },
+            child: SizeChangedLayoutNotifier(
+              key: const Key('qr-size-notifier'),
+              child: QRView(
+                key: _qrKey,
+                onQRViewCreated: _onQRViewCreated,
+                overlay: QrScannerOverlayShape(
+                  borderColor: Colors.red,
+                  borderRadius: 10,
+                  borderLength: 30,
+                  borderWidth: 10,
+                  cutOutSize: scanArea,
+                ),
+              ),
             ),
           ),
-          Align(
-              alignment: Alignment.bottomCenter,
-              child: FutureBuilder(
-                future: getFlashMode(),
-                builder: _buildFlashBtn,
-              ))
+          Positioned(
+            top: 30,
+            left: 15,
+            right: 15,
+            child: Row(
+              children: [
+                IconButton(
+                  icon: Icon(
+                    Icons.arrow_back_ios,
+                    size: 20,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    widget.context.backward();
+                  },
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 15,
+            right: 15,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _buildFlashBtn(
+                  context,
+                  _flashState,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 
-  Future<bool> getFlashMode() async {
-    bool isOpen = false;
-    try {
-      isOpen = await _controller.getFlashMode();
-    } catch (_) {}
-    return isOpen;
+  void _onQRViewCreated(QRViewController controller) {
+    this._controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      _controller.dispose();
+      if (mounted) {
+        Navigator.of(context).pop(scanData);
+      }
+    });
   }
 
-  Widget _buildFlashBtn(BuildContext context, AsyncSnapshot<bool> snapshot) {
-    return snapshot.hasData
+  Widget _buildFlashBtn(BuildContext context, bool flashOn) {
+    return flashOn != null
         ? Padding(
-      padding: EdgeInsets.only(
-          bottom: 24 + MediaQuery.of(context).padding.bottom),
-      child: IconButton(
-          icon: Icon(snapshot.data ? Icons.highlight : Icons.lightbulb_outline),
-          color: Colors.white,
-          iconSize: 46,
-          onPressed: () {
-            if (snapshot.data) {
-              _controller.setFlashMode(false);
-            } else {
-              _controller.setFlashMode(true);
-            }
-            setState(() {});
-          }),
-    )
+            padding: EdgeInsets.only(
+                bottom: 24 + MediaQuery.of(context).padding.bottom),
+            child: IconButton(
+              icon: Icon(flashOn ? Icons.highlight : Icons.lightbulb_outline),
+              color: Colors.white,
+              iconSize: 46,
+              onPressed: () {
+                _controller.toggleFlash();
+                _flashState = !_flashState;
+                if (mounted) {
+                  setState(() {});
+                }
+              },
+            ),
+          )
         : Container();
-  }
-}
-
-class ScanImageView extends StatefulWidget {
-  final Widget child;
-
-  const ScanImageView({Key key, this.child}) : super(key: key);
-
-  @override
-  _ScanImageViewState createState() => _ScanImageViewState();
-}
-
-class _ScanImageViewState extends State<ScanImageView>
-    with TickerProviderStateMixin {
-  AnimationController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = AnimationController(
-        vsync: this, duration: Duration(milliseconds: 1000));
-    controller.repeat(reverse: true);
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-        animation: controller,
-        builder: (BuildContext context, Widget child) => CustomPaint(
-          foregroundPainter:
-          _ScanPainter(controller.value, Colors.white, Colors.green),
-          child: widget.child,
-          willChange: true,
-        ));
-  }
-}
-
-class _ScanPainter extends CustomPainter {
-  final double value;
-  final Color borderColor;
-  final Color scanColor;
-
-  _ScanPainter(this.value, this.borderColor, this.scanColor);
-
-  Paint _paint;
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    if (_paint == null) {
-      initPaint();
-    }
-    double width = size.width;
-    double height = size.height;
-
-    double boxWidth = size.width * 2 / 3;
-    double boxHeight = height / 4;
-
-    double left = (width - boxWidth) / 2;
-    double top = boxHeight;
-    double bottom = boxHeight * 2;
-    double right = left + boxWidth;
-    _paint.color = borderColor;
-    final rect = Rect.fromLTWH(left, top, boxWidth, boxHeight);
-    canvas.drawRect(rect, _paint);
-
-    _paint.strokeWidth = 3;
-
-    Path path1 = Path()
-      ..moveTo(left, top + 10)
-      ..lineTo(left, top)
-      ..lineTo(left + 10, top);
-    canvas.drawPath(path1, _paint);
-    Path path2 = Path()
-      ..moveTo(left, bottom - 10)
-      ..lineTo(left, bottom)
-      ..lineTo(left + 10, bottom);
-    canvas.drawPath(path2, _paint);
-    Path path3 = Path()
-      ..moveTo(right, bottom - 10)
-      ..lineTo(right, bottom)
-      ..lineTo(right - 10, bottom);
-    canvas.drawPath(path3, _paint);
-    Path path4 = Path()
-      ..moveTo(right, top + 10)
-      ..lineTo(right, top)
-      ..lineTo(right - 10, top);
-    canvas.drawPath(path4, _paint);
-
-    _paint.color = scanColor;
-
-    final scanRect = Rect.fromLTWH(
-        left + 10, top + 10 + (value * (boxHeight - 20)), boxWidth - 20, 3);
-
-    _paint.shader = LinearGradient(colors: <Color>[
-      Colors.white54,
-      Colors.white,
-      Colors.white54,
-    ], stops: [
-      0.0,
-      0.5,
-      1,
-    ]).createShader(scanRect);
-    canvas.drawRect(scanRect, _paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
-  }
-
-  void initPaint() {
-    _paint = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 1
-      ..isAntiAlias = true
-      ..strokeCap = StrokeCap.round
-      ..strokeJoin = StrokeJoin.round;
   }
 }
